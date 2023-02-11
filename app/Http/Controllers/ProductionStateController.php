5ede90ea70b770c2634c01fe192890d2d5564fe9
AbdulH <haritani2002@gmail.com>
2023-02-11 11:34:03 +0100

    added comments

--- app/Http/Controllers/ProductionStateController.php
+++ app/Http/Controllers/ProductionStateController.php
@@ -6,70 +6,41 @@ class ProductionStateController extends Controller
 {
     public function index(): array
     {
-        $json = '[
-            {
-                "id": 123,
-                "saw": {
-                    "liggerg40": {
-                        "title": "Ligger G40",
-                        "amount": 2,
-                        "value": 1600
-                    },
-                    "staanderg54g56taatsdeur": {
-                        "title": "Staander G54 + G56 taatsdeur",
-                        "amount": 2,
-                        "value": 2396
-                    },
-                    "profielkleur": {
-                        "title": "PROFIELKLEUR: RAL 7032 Kiezel grijs"
-                    }
-                }
-            },
-            {
-                "id": 789,
-                "saw": {
-                    "staandersg70verstekdeur": {
-                        "title": "Staanders G70 verstek deur",
-                        "amount": 2,
-                        "value": 2785
-                    },
-                    "staanderg62g69deur": {
-                        "title": "Staander G69 deur",
-                        "amount": 1,
-                        "value": 2785
-                    },
-                    "liggerg40": {
-                        "title": "Ligger G40",
-                        "amount": 2,
-                        "value": 1600
-                    },
-                    "profielkleur": {
-                        "title": "PROFIELKLEUR: Leem"
-                    }
-                }
-            }
-        ]';
+        // Get the contents of the file located at storage/data/ProductieStaat.json
+        $json = file_get_contents(storage_path() . "/data/ProductieStaat.json");
 
+        // Decode the JSON data into an associative array
         $data = json_decode($json, true);
         $newJSON = [];
 
-        foreach ($data as $entry) { 
+        // Loop through each entry in the $data array
+        foreach ($data as $entry) {
+            // Check if the 'saw' and 'profielkleur' keys are present in the entry
             if (isset($entry['saw']['profielkleur'])) {
+                // Store the value of the 'title' key in the 'profielkleur' variable
                 $profilekleur = $entry['saw']['profielkleur']['title'];
+                // Loop through each value in the 'saw' key of the entry
                 foreach ($entry['saw'] as $key => $sawEntry) {
+                    // Check if the 'amount' key is present in the current value
                     if (isset($sawEntry['amount'])) {
+                        // Use a regular expression to extract any numbers from the current key
                         preg_match_all('~[0-9]+~', $key, $matches);
+                        // Loop through the matches
                         foreach ($matches as $number) {
+                            // Loop through each match
                             foreach ($number as $Gnumber) {
-                                $newJSON[$profilekleur][] = array('G' . $Gnumber => array('length'=> $sawEntry['value'],
-                                'count'=> $sawEntry['amount'],
-                            ));
+                                // Add the extracted information to the new JSON array
+                                $newJSON[$profilekleur][] = array('G' . $Gnumber => array(
+                                    'length' => $sawEntry['value'],
+                                    'count' => $sawEntry['amount'],
+                                ));
                             }
                         }
                     }
                 }
             }
         }
+        // Return the new JSON array
         return $newJSON;
     }
 }
